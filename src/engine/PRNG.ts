1f899e3a1f577c621cec3339cfacbf73f4cc7ae5
Damien Picard <dam.pic@free.fr>
2020-06-11 15:03:26 +0200

    Change PRNG to Mulberry32 instead of Lehmer LCG

--- src/engine/PRNG.ts
+++ src/engine/PRNG.ts
@@ -1,16 +1,22 @@
-// Taken from https://gist.github.com/blixt/f17b47c62508be59987b
+// Taken from https://github.com/bryc/code/blob/master/jshash/PRNGs.md
 // Ink uses a seedable PRNG of which there is none in native javascript.
 export class PRNG {
-  private seed: number;
+  public seed: number;
 
   constructor(seed: number) {
-    this.seed = seed % 2147483647;
-    if (this.seed <= 0) this.seed += 2147483646;
+    this.seed = seed;
   }
+
   public next(): number {
-    return (this.seed = (this.seed * 16807) % 2147483647);
+    let a = this.seed;
+    a |= 0;
+    a = (a + 0x6d2b79f5) | 0;
+    let t = Math.imul(a ^ (a >>> 15), 1 | a);
+    t = (t + Math.imul(t ^ (t >>> 7), 61 | t)) ^ t;
+    this.seed = a;
+    return (t ^ (t >>> 14)) >>> 0;
   }
   public nextFloat(): number {
-    return (this.next() - 1) / 2147483646;
+    return this.next() / 4294967296;
   }
 }
